a34bc9ccaae134eefc267301d5fc5906850d68d1
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-01-17 15:14:51 +0300

    Create ParallelAggregation.cs

---
+++ ParallelAggregation.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AlbahariComTests
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            Stopwatch timer = new Stopwatch();
+            IEnumerable<int> nums = ParallelEnumerable.Range(0, int.MaxValue);
+            long sum = 0;
+
+            timer.Start();
+            sum = nums.AsParallel().Aggregate(
+                () => (long)0,                              // seedFactory: first value of localTotal
+                (localTotal, n) => localTotal + n,          // updateAccumulatorFunc:adding values to local total
+                (mainTot, localTot) => mainTot + localTot,  // combineAccumulatorFunc:interlock add at mainTotal
+                finalResult => finalResult);                // resultSelector:last return value, sum
+            timer.Stop();
+            
+            //less than 9 secs
+            Console.WriteLine(timer.Elapsed.TotalMilliseconds);
+            Console.WriteLine(sum);
+        }
+    }
+}
