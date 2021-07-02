c92652011bf044cae7a812d2fd3551946ae9f8ef
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-07-02 20:35:56 +0300

    Update FindMinMax.cs

--- FindMinMax.cs
+++ FindMinMax.cs
@@ -1,3 +1,11 @@
+public static Main(string[] args)
+{
+      //int32 minimum , int32 maximum
+      var (minimum,maximum) = FindMinMax(-1,2,33,4,-10,6);
+      System.Console.WriteLine($"Min:{minimum} Max:{maximum}");
+}
+
+
 static (int min, int max)  FindMinMax(params int[] input)
 {
       if (input is null || input.Length == 0)
