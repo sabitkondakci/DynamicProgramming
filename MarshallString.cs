b39903a8bdad717fa127b5ed92ab709e6ecab185
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-08-03 14:54:38 +0300

    Update MarshallString.cs

--- MarshallString.cs
+++ MarshallString.cs
@@ -54,3 +54,64 @@ public static class ExtensionString
         return tmpString.ToString();
     }
 }
+
+
+
+// Unicode 
+public static class ExtensionString
+{
+    public static string CustomReverse(this string script)
+    {
+       
+        string result = null;
+        string strValue = script;
+        int strLength = strValue.Length;
+        
+        
+        // Allocate HGlobal memory for source and destination strings
+        IntPtr sourcePtr = Marshal.StringToHGlobalUni(strValue);
+        IntPtr destPtr = Marshal.AllocHGlobal(strLength + 1);
+
+        // The unsafe section where byte pointers are used.
+        if (sourcePtr != IntPtr.Zero && destPtr != IntPtr.Zero)
+        {
+            unsafe
+            {
+                ushort *src = (ushort *)sourcePtr.ToPointer();
+                ushort *dst = (ushort *)destPtr.ToPointer();
+
+                if (strLength > 0)
+                {
+                    // set the source pointer to the end of the string
+                    // to do a reverse copy.
+                    src += strLength - 1;
+                    while (strLength-- > 0)
+                    {
+                        *dst++ = *src--;
+                    }
+                
+                    *dst = 0;
+                }
+            }
+            
+            result = Marshal.PtrToStringUni(destPtr);
+        }
+        
+        Marshal.FreeHGlobal(sourcePtr);
+        Marshal.FreeHGlobal(destPtr);
+
+        return result;
+    }
+    
+    public static string LinqReverse(this string script)
+    {
+        var tmpString = new StringBuilder();
+        var reversedStr = script.Reverse();
+        foreach (var c in reversedStr)
+        {
+            tmpString.Append(c);
+        }
+
+        return tmpString.ToString();
+    }
+}
