bae11c87bf6955491c48a856d4e534f8f8e53c3e
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-08-03 15:43:38 +0300

    Update MarshallString.cs

--- MarshallString.cs
+++ MarshallString.cs
@@ -57,52 +57,57 @@ public static class ExtensionString
 
 
 
-// Unicode 
+// Unicode & try-finally
 public static class ExtensionString
 {
     public static string CustomReverse(this string script)
     {
-       
         string result = null;
         string strValue = script;
         int strLength = strValue.Length;
-        
-        
+
+
         // Allocate HGlobal memory for source and destination strings
         IntPtr sourcePtr = Marshal.StringToHGlobalUni(strValue);
-        IntPtr destPtr = Marshal.AllocHGlobal(strLength + 1);
+        IntPtr destPtr = Marshal.StringToHGlobalUni(strValue);
 
         // The unsafe section where byte pointers are used.
-        if (sourcePtr != IntPtr.Zero && destPtr != IntPtr.Zero)
+        try
         {
-            unsafe
+            if (sourcePtr != IntPtr.Zero && destPtr != IntPtr.Zero)
             {
-                ushort *src = (ushort *)sourcePtr.ToPointer();
-                ushort *dst = (ushort *)destPtr.ToPointer();
-
-                if (strLength > 0)
+                unsafe
                 {
-                    // set the source pointer to the end of the string
-                    // to do a reverse copy.
-                    src += strLength - 1;
-                    while (strLength-- > 0)
-                    {
-                        *dst++ = *src--;
-                    }
-                
-                    *dst = 0;
-                }
-            }
-            
-            result = Marshal.PtrToStringUni(destPtr);
-        }
-        
-        Marshal.FreeHGlobal(sourcePtr);
-        Marshal.FreeHGlobal(destPtr);
+                    ushort* src = (ushort*)sourcePtr.ToPointer();
+                    ushort* dst = (ushort*)destPtr.ToPointer();
 
-        return result;
+                    if (strLength > 0)
+                    {
+                        // set the source pointer to the end of the string
+                        // to do a reverse copy.
+                        src += strLength - 1;
+                        while (strLength-- > 0)
+                        {
+                            *dst++ = *src--;
+                        }
+
+                        *dst = 0;
+                    }
+                }
+
+                result = Marshal.PtrToStringUni(destPtr);
+            }
+
+            return result;
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(sourcePtr);
+            Marshal.FreeHGlobal(destPtr);    
+        }
+
     }
-    
+
     public static string LinqReverse(this string script)
     {
         var tmpString = new StringBuilder();
