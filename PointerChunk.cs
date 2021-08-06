e5c498510f1fe89195697bbe3d73a35ed94aaf20
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-08-06 22:42:14 +0300

    Update PointerChunk.cs

--- PointerChunk.cs
+++ PointerChunk.cs
@@ -1,3 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Web;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Iced.Intel;
+using Encoder = System.Text.Encoder;
+
+class BestPractices
+{
+    public static void Main()
+    {
+        //var chunk = new PointerChunk(Enumerable.Range(1, 1000).ToArray(), 200);
+        // for (int i = 0; i < chunk.Length; i++)
+        // {
+        //     var temp = chunk[i];
+        // }
+       
+        var a = BenchmarkRunner.Run<ChunkBenchmark>();
+
+    }
+    
+}
+
+public static class ArrayExtensions
+{
+    public static IEnumerable<int[]> Chunk(this int[] array, int size)
+    {
+        while (array.Any())
+        {
+            yield return array.Take(size).ToArray();
+            array = array.Skip(size).ToArray();
+        }   
+    }
+}
+
+[MemoryDiagnoser]
+public class ChunkBenchmark
+{
+    public int[] array;
+
+    [GlobalSetup]
+    public void InitializeArray()
+    {
+        array = Enumerable.Range(1, 10_000_000).Where(x => x % 2 == 0).ToArray();
+    }
+
+    [Benchmark]
+    public void PointerChunkTest()
+    { 
+        var pointerChunk = new PointerChunk(array, 1_000);
+        for (int i = 0; i < 100; i++)
+        {
+            var a = pointerChunk[3];
+        }
+    }
+
+    [Benchmark]
+    public void LinqChunkTest()
+    {
+        var chunk = array.Chunk(1_000);
+        for (int i = 0; i < 100; i++)
+        {
+            var a = chunk[3];
+        }
+    }
+}
+
 public sealed class PointerChunk : IDisposable
 { 
     private IntPtr[] _chunkList;
@@ -14,7 +89,7 @@ public sealed class PointerChunk : IDisposable
     }
     
     public int Length => _chunkListSize;
-    
+
     public int[] this[int i]
     {
         get
@@ -29,44 +104,44 @@ public sealed class PointerChunk : IDisposable
             else
             {
                 _partialList = new int[_chunkSize];
-                
-                for (ushort j = 0; j < _chunkSize; j++)
+                var tempPtr = _chunkList[i];
+                for (int j = 0; j < _chunkSize; j++)
                 {
-                    var tempPtr = IntPtr.Add(_chunkList[i], size * j); // IntPtr newPtr = IntPtr.Subtract(_chunkList[i], size * j) inverse iteration.
-                    
-                    
-                    var value = Marshal.ReadInt32(tempPtr);
+                    var value = Marshal.ReadInt32(tempPtr, size * j);
                     _partialList[j] = value;
                 }
-                
+
                 return _partialList;
             }
         }
     }
-    
-    private unsafe IntPtr[] Chunk(int[] largeArray,int chunkSize)
+
+    public IntPtr[] Chunk(int[] largeArray,int chunkSize)
     {
         IntPtr[] list;
         var tempArr = largeArray;
-        
-        fixed (int* ptr = tempArr)
-        {
-            IntPtr iPtr = new IntPtr(ptr);
-            int arrLength = largeArray.Length;
-            
-            int noReminderSize = arrLength / chunkSize;
-            int reminderSize = arrLength / chunkSize + 1;
-            int remainder = arrLength % chunkSize;
-            
-            int bounce = sizeof(int);
-            int loopSize = remainder == 0 ? noReminderSize : reminderSize;
-            _lastLoopSize = remainder == 0 ? chunkSize : remainder;
 
-            list = new IntPtr[loopSize];
-            for (int i = 0; i < loopSize; i++)
+        unsafe
+        {
+            fixed (int* ptr = tempArr)
             {
-                IntPtr tempPtr = IntPtr.Add(iPtr, i * bounce * chunkSize);
-                list[i] = tempPtr;
+                IntPtr iPtr = new IntPtr(ptr);
+                int arrLength = largeArray.Length;
+            
+                int noReminderSize = arrLength / chunkSize;
+                int reminderSize = arrLength / chunkSize + 1;
+                int remainder = arrLength % chunkSize;
+            
+                int bounce = sizeof(int);
+                int loopSize = remainder == 0 ? noReminderSize : reminderSize;
+                _lastLoopSize = remainder == 0 ? chunkSize : remainder;
+
+                list = new IntPtr[loopSize];
+                for (int i = 0; i < loopSize; i++)
+                {
+                    IntPtr tempPtr = IntPtr.Add(iPtr, i * bounce * chunkSize);
+                    list[i] = tempPtr;
+                }
             }
         }
 
@@ -81,3 +156,4 @@ public sealed class PointerChunk : IDisposable
         }
     }
 }
+
