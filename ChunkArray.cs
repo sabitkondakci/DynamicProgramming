8f5b6a77abb847946f313e1bcade3eb0bb83f6d3
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-08-18 20:11:12 +0300

    Delete ChunkArray.cs

--- ChunkArray.cs
+++
@@ -1,8 +0,0 @@
-public  IEnumerable<int[]> Chunk(int[] array, int size)
-{
-    while (array.Any())
-    {
-        yield return array.Take(size).ToArray();
-        array = array.Skip(size).ToArray();
-    }   
-}
