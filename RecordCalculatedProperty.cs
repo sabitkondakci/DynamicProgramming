1de2936404f7edab70275bd5f20d1cc26725b2aa
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-09-01 17:05:55 +0300

    Create RecordCalculatedProperty.cs

---
+++ RecordCalculatedProperty.cs
@@ -0,0 +1,17 @@
+void Main()
+{
+	var point = new Point (1, 1);
+	point.DistanceFromOrigin.Dump();
+	
+	var point2 = point with { Y = 10 };
+	point2.DistanceFromOrigin.Dump();    // Works
+}
+
+record Point (double X, double Y)
+{
+	double? _distance;
+	public double DistanceFromOrigin => _distance ??= Math.Sqrt (X * X + Y * Y);
+
+	protected Point (Point other) => (X, Y) = (other.X, other.Y); 
+ // protected Point (Point other) => (X, Y) = other;
+}
