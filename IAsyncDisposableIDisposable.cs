70b26a3cca2977eeb0145a0bd8d5b0ce85d52535
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-10-19 21:14:20 +0300

    Update IAsyncDisposableIDisposable.cs

--- IAsyncDisposableIDisposable.cs
+++ IAsyncDisposableIDisposable.cs
@@ -41,3 +41,59 @@ public class ExampleAsyncDisposable : IAsyncDisposable, IDisposable
 		_jsonWriter = null;
 	}
 }
+
+
+
+// second approach
+
+class ExampleConjunctiveDisposableusing : IDisposable, IAsyncDisposable
+{
+	IDisposable _disposableResource = new MemoryStream();
+	IAsyncDisposable _asyncDisposableResource = new MemoryStream();
+
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+
+	public async ValueTask DisposeAsync()
+	{
+		await DisposeAsyncCore();
+
+		Dispose(disposing: false);
+		GC.SuppressFinalize(this);
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (disposing)
+		{
+			_disposableResource?.Dispose();
+			(_asyncDisposableResource as IDisposable)?.Dispose();
+		}
+
+		_disposableResource = null;
+		_asyncDisposableResource = null;
+	}
+
+	protected virtual async ValueTask DisposeAsyncCore()
+	{
+		if (_asyncDisposableResource is not null)
+		{
+			await _asyncDisposableResource.DisposeAsync().ConfigureAwait(false);
+		}
+
+		if (_disposableResource is IAsyncDisposable disposable)
+		{
+			await disposable.DisposeAsync().ConfigureAwait(false);
+		}
+		else
+		{
+			_disposableResource?.Dispose();
+		}
+
+		_asyncDisposableResource = null;
+		_disposableResource = null;
+	}
+}
