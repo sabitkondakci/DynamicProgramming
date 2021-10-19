c10970561891c3cfdbb999694d2f27276c3c3707
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-10-19 21:05:03 +0300

    Create IAsyncDisposableIDisposable.cs

---
+++ IAsyncDisposableIDisposable.cs
@@ -0,0 +1,47 @@
+public class ExampleAsyncDisposable : IAsyncDisposable, IDisposable
+{
+	private Utf8JsonWriter _jsonWriter;
+
+	public ExampleAsyncDisposable()
+	{
+		_jsonWriter = new(new MemoryStream());
+	}
+	
+	public async Task FlushAsync()
+	{
+		await _jsonWriter.FlushAsync();
+	}
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+
+	public async ValueTask DisposeAsync()
+	{
+		await DisposeAsyncCore();
+
+		Dispose(false); // dispose unmanaged objects
+		GC.SuppressFinalize(this);
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (disposing)
+		{
+			_jsonWriter?.Dispose();
+		}
+
+		_jsonWriter = null;
+	}
+
+	protected virtual async ValueTask DisposeAsyncCore()
+	{
+		if (_jsonWriter is not null)
+		{
+			await _jsonWriter.DisposeAsync().ConfigureAwait(false);
+		}
+
+		_jsonWriter = null;
+	}
+}
