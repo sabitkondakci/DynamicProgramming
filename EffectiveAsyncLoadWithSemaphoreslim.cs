47411083951cfb9b116ce8a998ddb58d2ebe6b66
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-10-25 16:11:33 +0300

    Update EffectiveAsyncLoadWithSemaphoreslim.cs

--- EffectiveAsyncLoadWithSemaphoreslim.cs
+++ EffectiveAsyncLoadWithSemaphoreslim.cs
@@ -7,16 +7,14 @@ using System.Net.NetworkInformation;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using namespace ConsoleDemo;
 
-namespace ConsoleDemo
+class EffectiveLoad
 {
-
-	class EffectiveLoad
+	static async Task Main()
 	{
-		static async Task Main()
-		{
-			#region string[] list
-			string[] list = {
+		#region string[] list
+		string[] list = {
 						"www.youtube.com",
 						"www.facebook.com		",
 						"www.baidu.com			",
@@ -119,109 +117,113 @@ namespace ConsoleDemo
 						"www.answers.yahoo.com    "
 															};
 
-			list = list.Select(l => l.Trim()).ToArray();
-			#endregion
+		list = list.Select(l => l.Trim()).ToArray();
+		list = list.Concat(list).ToArray();
+		#endregion
 
-			// pass a timeout accordingly
-			await PrintPingList(list, 300);
-		}
-
-		public readonly struct NetworkInfo
-		{
-			public string Website { get; init; }
-			public IPAddress Address { get; init; }
-			public IPStatus Status { get; init; }
-			public long RoundTripTime { get; init; }
-		}
-
-		public static async Task PrintPingList(string[] sites, int timeout = -1)
-		{
-			using CancellationTokenSource source = new(timeout);
-
-			try
-			{
-				// PingAsync(sites,source.Token) is to cancel running tasks
-				// pingResultList.WithCancellation(source.Token) is to cancel IAsyncEnumerable's enumerator ==> IAsyncEnumerable <out T>.GetAsyncEnumerator(CancellationToken)
-
-				var pingResultList = PingAsync(sites,source.Token);
-				await Print(pingResultList,source.Token);
-			}
-			catch (OperationCanceledException)
-			{
-				Console.WriteLine("Operation has been cancelled!");
-				// log it!
-			}
-		}
-		
-		public static async Task Print(IAsyncEnumerable<NetworkInfo> pingResultList,
-		CancellationToken token = default)
-		{
-			await foreach (var pingResult in pingResultList
-				.WithCancellation(token).ConfigureAwait(false))
-			{
-				Console.WriteLine($"Website:{pingResult.Website}\n"
-			+ $"Address:{pingResult.Address}\nStatus:{pingResult.Status}\n"
-			+ $"RoundTripTime: {pingResult.RoundTripTime}");
-
-				Console.WriteLine();
-				
-				// catch the cancellation quicker
-				if(token.IsCancellationRequested)
-					throw new OperationCanceledException();
-			}
-		}
-		
-		public static async IAsyncEnumerable<NetworkInfo> PingAsync(string[] websites, [EnumeratorCancellation] CancellationToken cancellationToken = default)
-		{
-			var length = websites.Length;
-			var taskNetworkInfo = new Queue<Task<NetworkInfo>>();
-
-			// allow 10 worker thread in at a time, this will prevent the thread exhaustion.
-			using (var semaphoreTen = new SemaphoreSlim(10))
-			{
-				for (int i = 0; i < length; i++)
-				{
-					var k = i;
-					NetworkInfo info = new();
-					taskNetworkInfo.Enqueue(Task.Run(async () =>
-					{
-					// semaphoreTen will let only 10 worker thread to get their job done at a time.
-						await semaphoreTen.WaitAsync(cancellationToken);
-						Ping ping = new();
-
-						try
-						{
-							var pingResult = await ping.SendPingAsync(websites[k], 500);
-
-							info = new NetworkInfo
-							{
-								Address = pingResult.Address,
-								Website = websites[k],
-								RoundTripTime = pingResult.RoundtripTime,
-								Status = pingResult.Status
-							};
-						}
-						catch { /* log the errors */}
-						finally 
-						{
-							if(!cancellationToken.IsCancellationRequested)
-								semaphoreTen.Release();
-								
-						 	ping.Dispose();
-						}
-						return info;
-					}, cancellationToken));
-				}
-
-				while (taskNetworkInfo.TryDequeue(out var networkInfo))
-				{
-					yield return await networkInfo;				
-				}
-				
-			}
-
-		}
-
+		// pass a timeout accordingly
+		await PrintPingList(list);
 	}
-    	
+
+	public readonly struct NetworkInfo
+	{
+		public string Website { get; init; }
+		public IPAddress Address { get; init; }
+		public IPStatus Status { get; init; }
+		public long RoundTripTime { get; init; }
+	}
+
+	public static async Task PrintPingList(string[] sites, int timeout = -1)
+	{
+		using CancellationTokenSource source = new(timeout);
+
+		try
+		{
+			// PingAsync(sites,source.Token) is to cancel running tasks
+			// pingResultList.WithCancellation(source.Token) is to cancel IAsyncEnumerable's enumerator ==> IAsyncEnumerable <out T>.GetAsyncEnumerator(CancellationToken)
+
+			var pingResultList = PingAsync(sites, source.Token);
+			await Print(pingResultList, source.Token);
+		}
+		catch (OperationCanceledException)
+		{
+			Console.WriteLine("Operation has been cancelled!");
+			// log it!
+		}
+	}
+
+	public static async Task Print(IAsyncEnumerable<NetworkInfo> pingResultList,
+	CancellationToken token = default)
+	{
+		await foreach (var pingResult in pingResultList
+			.WithCancellation(token).ConfigureAwait(false))
+		{
+			Console.WriteLine($"Website:{pingResult.Website}\n"
+		+ $"Address:{pingResult.Address}\nStatus:{pingResult.Status}\n"
+		+ $"RoundTripTime: {pingResult.RoundTripTime}");
+
+			Console.WriteLine();
+
+			// catch the cancellation quicker
+			if (token.IsCancellationRequested)
+				throw new OperationCanceledException();
+		}
+	}
+	public static async IAsyncEnumerable<NetworkInfo> PingAsync(string[] websites, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+	{
+		var length = websites.Length;
+		var taskNetworkInfo = new Queue<Task<NetworkInfo>>();
+
+		// allow 10 worker thread in at a time, this will prevent the thread exhaustion.
+		using (var semaphoreTen = new SemaphoreSlim(10))
+		{
+			for (int i = 0; i < length; i++)
+			{
+				var k = i;
+				NetworkInfo info = new();
+				await semaphoreTen.WaitAsync(cancellationToken);
+
+				taskNetworkInfo.Enqueue(Task.Run(async () =>
+				{
+					// semaphoreTen will let only 10 worker thread to get their job done at a time.
+					Ping ping = new();
+
+					try
+					{
+						var pingResult = await ping.SendPingAsync(websites[k], 500);
+
+						info = new NetworkInfo
+						{
+							Address = pingResult.Address,
+							Website = websites[k],
+							RoundTripTime = pingResult.RoundtripTime,
+							Status = pingResult.Status
+						};
+					}
+					catch { /* log the errors */}
+					finally
+					{
+						ping.Dispose();
+					}
+
+					return info;
+
+				}, cancellationToken));
+
+				// flush 10 items to IAsyncEnumerable at a time
+				int qFlush = (i + 1) % 10;
+				if (qFlush == 0 || i >= length - 10)
+				{
+					while (taskNetworkInfo.TryDequeue(out var networkInfo))
+					{
+						if (!cancellationToken.IsCancellationRequested)
+							semaphoreTen.Release();
+
+						yield return await networkInfo;
+					}
+				}
+			}
+		}
+	}
 }
+
