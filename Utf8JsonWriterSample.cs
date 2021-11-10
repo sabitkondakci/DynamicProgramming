90585838b597d3750eca972f0453f758a430a3c8
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-11-10 15:52:17 +0300

    Create Utf8JsonWriterSample.cs

---
+++ Utf8JsonWriterSample.cs
@@ -0,0 +1,86 @@
+async Task Main()
+{
+	var options = new JsonWriterOptions
+	{
+		Indented = true
+	};
+	
+	// Provides methods to transform UTF-8
+	// or UTF-16 encoded text into a form that is suitable for JSON.
+	var jsonEncodedText = JsonEncodedText.
+		Encode("sampleArray",JavaScriptEncoder.UnsafeRelaxedJsonEscaping);
+		
+	
+	await using (var stream = new MemoryStream())
+	{
+		await using (var writer = new Utf8JsonWriter(stream, options))
+		{
+			writer.WriteStartObject();
+			writer.WriteString("start-date", DateTimeOffset.UtcNow);
+			writer.WriteNumber("array-length", 2);
+			writer.WriteBoolean("is-still-valid",true);
+			
+			writer.WriteCommentValue("sample arrays are great!");
+			writer.WriteStartArray(jsonEncodedText.EncodedUtf8Bytes);
+				writer.WriteStartObject();
+				writer.WriteString("name", "Ahmet");
+				writer.WriteNumber("age", 14);
+				writer.WriteEndObject();
+
+				writer.WriteStartObject();
+				writer.WriteString("name", "Elif");
+				writer.WriteNumber("age", 11);
+				writer.WriteEndObject();
+				
+				writer.WriteCommentValue("Array Values");
+				// boolean in a json array
+				writer.WriteBooleanValue(false); 
+				// null in a json array
+				writer.WriteNullValue();
+				// string value in a json array
+				writer.WriteStringValue("test string value"); 
+				// number value in a json array
+				writer.WriteNumberValue(999); 
+				
+			writer.WriteEndArray();
+			
+			writer.WriteNull("nullProperty");
+			
+			writer.WriteEndObject();
+			await writer.FlushAsync();
+		}
+		
+		// store it to a file
+		using var fileStream = File.Create(@"C:\Users\fenko\Desktop\sample.json");
+		await fileStream.WriteAsync(stream.ToArray());
+	}
+}
+
+// OUTPUT:
+
+//{
+//  "start-date": "2021-11-10T12:38:21.2030153+00:00",
+//  "array-length": 2,
+//  "is-still-valid": true
+//  /*sample arrays are great!*/,
+//  "sampleArray": [
+//    {
+//      "name": "Ahmet",
+//      "age": 14
+//    },
+//    {
+//	"name": "Elif",
+//      "age": 11
+//
+//	}
+//    /*Array Values*/,
+//    false,
+//    null,
+//    "test string value",
+//    999
+//  ],
+//  "nullProperty": null
+//}
+
+
+
