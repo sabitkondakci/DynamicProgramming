c09ddaa983aa983c2345cecee0249ec016eb3392
Sabit Kondakçı <kondakcisabit@gmail.com>
2022-05-12 21:34:36 +0300

    Update LINQEqualityComparer.cs

--- LINQEqualityComparer.cs
+++ LINQEqualityComparer.cs
@@ -2,13 +2,13 @@ void Main()
 {
 	var itemList = new List<Tool>()
 	{
-		new Tool(){ },
-		null,
-		null,
-		new Tool() { }
+		new Tool(){ ID = 1, Name = "Pump", ProductionDate = DateTime.Today },
+		new Tool() { ID = 2, Name = "Toy", ProductionDate = DateTime.Today.AddDays(-63) },
+		new Tool() { ID = 2, Name = "Toy", ProductionDate = DateTime.Today.AddDays(-63) },
+		new Tool() { ID = 3, Name = "Faucet", ProductionDate = DateTime.Today.AddDays(-3) }
 	};
 
-	var distList = itemList.Distinct(new ToolComparer());
+	var distList = itemList.Distinct(ToolComparer.GetInstance());
 	distList.Dump();
 	
 }
@@ -22,6 +22,15 @@ public class Tool
 
 public class ToolComparer : IEqualityComparer<Tool>
 {
+
+	public static ToolComparer GetInstance() => _comparer.Value;
+	
+	private ToolComparer() { }
+
+	// Lazy<> class ensures thread safety , .Net 4.0+
+	private static readonly Lazy<ToolComparer> _comparer =
+		new Lazy<ToolComparer>(() => new ToolComparer(), LazyThreadSafetyMode.ExecutionAndPublication);
+		
 	// Second, once a similar GetHashCode returns, Eqauls() is checked.
 	public bool Equals(Tool x, Tool y)
 	{
