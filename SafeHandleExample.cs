8d23b535574d10fa947c3d4c0be9702cf23bcda7
Sabit Kondakçı <kondakcisabit@gmail.com>
2022-05-24 00:26:39 +0300

    Update SafeHandleExample.cs

--- SafeHandleExample.cs
+++ SafeHandleExample.cs
@@ -23,3 +23,59 @@ class BaseClassWithSafeHandle : IDisposable
 		}
 	}
 }
+
+// When a base class implements SafeHandle
+class DerivedClassWithSafeHandle : BaseClassWithSafeHandle
+{
+    // To detect redundant calls
+    private bool _disposedValue;
+
+    // Instantiate a SafeHandle instance.
+    private SafeHandle _safeHandle = new SafeFileHandle(IntPtr.Zero, true);
+
+    // Protected implementation of Dispose pattern.
+    protected override void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                _safeHandle.Dispose();
+            }
+
+            _disposedValue = true;
+        }
+
+        // Call base class implementation, Idempotent!
+        base.Dispose(disposing);
+    }
+}
+
+
+// When a base class implements Finalizer pattern
+class DerivedClassWithFinalizer : BaseClassWithFinalizer
+{
+    // To detect redundant calls
+    private bool _disposedValue;
+
+    ~DerivedClassWithFinalizer() => this.Dispose(false);
+
+    // Protected implementation of Dispose pattern.
+    protected override void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects).
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+            // TODO: set large fields to null.
+            _disposedValue = true;
+        }
+
+        // Call the base class implementation.
+        base.Dispose(disposing);
+    }
+}
